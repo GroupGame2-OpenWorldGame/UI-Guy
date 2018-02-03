367039c36fb3bcbab6c673426b3c3abe8b6703ab
MichaelAlbright <michaelalbright25@yahoo.com>
2018-02-03 11:45:57 -0500

    Created and added stuff

---
+++ Assets/ChangeMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenu : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] menues;
+
+	public void MenuChange()
+	{
+		foreach (GameObject temp in menues) {
+			temp.SetActive (!temp.activeInHierarchy);
+		}
+	}
+}
---
+++ Assets/NPCMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour {
+
+	[SerializeField]
+	private Transform destination;
+
+	private NavMeshAgent navAgent;
+
+	[SerializeField]
+	private bool isMovingTarget;
+
+	// Use this for initialization
+	void Start () {
+		navAgent = this.GetComponent<NavMeshAgent> ();
+
+		if (navAgent == null) {
+			Debug.LogError ("The nav mesh agent is not attatched to " + gameObject.name);
+		} else {
+			SetDestination ();
+		}
+	}
+
+	void Update()
+	{
+		if (isMovingTarget) {
+			SetDestination ();
+		}
+	}
+
+	void SetDestination () {
+		if (destination != null) {
+			Vector3 targetVector = destination.transform.position;
+			navAgent.SetDestination (targetVector);
+		}
+	}
+}
