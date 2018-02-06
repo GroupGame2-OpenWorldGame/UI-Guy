71fa7d31fd35a0dcea2f0ecaf8c45a172f561385
MichaelAlbright <michaelalbright25@yahoo.com>
2018-02-06 17:23:06 -0500

    working on npc patrol

--- Assets/NPCMove.cs
+++ Assets/NPCMove.cs
@@ -13,12 +13,27 @@ public class NPCMove : MonoBehaviour {
 	[SerializeField]
 	private bool isMovingTarget;
 
+	[SerializeField]
+	private GameObject[] patrolPoints;
+	[SerializeField]
+	private int currentPoint;
+	[SerializeField]
+	private bool randomPatrol;
+	[SerializeField]
+	private bool patroling;
+
 	// Use this for initialization
 	void Start () {
 		navAgent = this.GetComponent<NavMeshAgent> ();
 
+		if (randomPatrol && !patroling) {
+			patroling = true;
+		}
+
 		if (navAgent == null) {
 			Debug.LogError ("The nav mesh agent is not attatched to " + gameObject.name);
+		} else if (patroling) {
+			ChangePatrol ();
 		} else {
 			SetDestination ();
 		}
@@ -29,12 +44,40 @@ public class NPCMove : MonoBehaviour {
 		if (isMovingTarget) {
 			SetDestination ();
 		}
+
+		if (currentPoint >= patrolPoints.Length) {
+			currentPoint = 0;
+		}
 	}
 
 	void SetDestination () {
-		if (destination != null) {
+		if (destination != null && !patroling) {
 			Vector3 targetVector = destination.transform.position;
 			navAgent.SetDestination (targetVector);
+		} else if (destination != null && patroling) {
+			navAgent.SetDestination (patrolPoints [currentPoint].transform.position);
 		}
+	}
+
+//	void OnTriggerEnter (Collider other)
+//	{
+//		if (other.CompareTag("PatrolPoint")) {
+//			ChangePatrol ();
+//			other.gameObject.SetActive (false);
+//		}
+//		Debug.Log ("enter " + other.name);
+//	}
+
+	public void ChangePatrol()
+	{
+		int tempPoint;
+		if (randomPatrol) {
+			tempPoint = Random.Range (0, patrolPoints.Length);
+			currentPoint = tempPoint;
+		} else {
+			tempPoint = currentPoint;
+			currentPoint++;
+		}
+		navAgent.SetDestination (patrolPoints [tempPoint].transform.position);
 	}
 }
---
+++ Assets/TriggerEnter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnter : MonoBehaviour {
+
+	[SerializeField]
+	private NPCMove theNPC;
+
+	void OnColliderEnter (Collision other)
+	{
+		if (other.gameObject.tag == "NPC") {
+			theNPC.ChangePatrol ();
+			this.gameObject.tag = "OldPatrolPoint";
+		}
+		Debug.Log ("Enter " + other.gameObject.name);
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.CompareTag("NPC")) {
+			this.gameObject.tag = "PatrolPoint";
+		}
+		Debug.Log ("Exit " + other.name);
+	}
+}
